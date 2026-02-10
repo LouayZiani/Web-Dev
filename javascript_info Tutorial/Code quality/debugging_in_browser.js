e873bfdf4e73008b2cf0dcc1dfd3d34e3470f967
Louay Ziani <156789538+LouayZiani@users.noreply.github.com>
2026-02-11 00:30:46 +0500

    Delete javascript_info Tutorial directory

--- javascript_info Tutorial/Code quality/debugging_in_browser.js
+++
@@ -1,4 +0,0 @@
-// output smth to console
-for (let i = 0; i < 5; i++) {
-  console.log("value,", i);
-}
--- javascript_info Tutorial/Data types/primitives.js
+++
@@ -1,8 +0,0 @@
-/*task: Consider the following code:
-What do you think, will it work? What will be shown? */
-
-let str = "Hello";
-
-str.test = 5;
-
-alert(str.test); // error, cuz its a primitive not an object, it cant store extra data
