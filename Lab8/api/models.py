9c7c4e8fb4b9fd9b41a3a9ac07348eed8ed56191
Louay <louzianiay25@gmail.com>
2026-03-26 00:10:29 +0500

    Lab8 (Django backend)

---
+++ Lab8/api/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    #name, price, description, count, is_active, category
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    description = models.TextField()
+    count = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+
+    def __str__(self):
+        return self.name
